c5d9609b6686437bb351d56c49d9bcdc49c4a1db
zxccursed <asobolev925@gmail.com>
2026-02-25 19:45:04 +0400

    day

--- database/migrations/2025_11_10_155339_create_posts_table.php
+++ database/migrations/2025_11_10_155339_create_posts_table.php
@@ -16,7 +16,7 @@ return new class extends Migration {
             $table->string("content", 255);
             $table->string("tags")->nullable();
             $table->string("files")->nullable();
-            $table->string("description")->nullable();
+            $table->text("description")->nullable();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('departament_id');
             $table->timestamps();
--- database/migrations/2025_11_26_113802_entities.php
+++ database/migrations/2025_11_26_113802_entities.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -17,7 +16,7 @@ return new class extends Migration
             $table->string('path');
             $table->string('idInBestiary');
             $table->mediumInteger("initiative");
-            $table->string("description");
+            $table->text("description");
             $table->bigInteger('size')->default(1);
             $table->unsignedBigInteger("user_id")->nullable();
             $table->unsignedBigInteger('source_id');
